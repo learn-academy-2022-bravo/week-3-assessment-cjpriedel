a9ab248ab01359391db3ab4f544cf5ef2ebaa76f
cjpriedel <cjpriedel@gmail.com>
2022-04-11 00:45:23 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,7 +13,37 @@
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
+//Pseudo Code 
+// take a # > 2 and returns an array that length
+// reference fib seq 
+// let x = 0, y = 1, z = x+y
+// loop and add each new iteration to an array to log fib series
+// use .unshift/.push/ maybe .map? 
+
+
+const fibLog = (num) => {
+    let n1 = 0, n2 = 1, nextNumber
+    let fibArray = []
+    for (i = 1; i <= num; i++) 
+    {
+        nextNumber = n1+n2
+        n1 = n2
+        n2 = nextNumber
+        fibArray.push(n1)
+        
+    }
+      return fibArray
+}
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("fibLog", () => {
+    it("returns an array containing the numbers of the Fibonacci sequence up to the number entered as an argument", () => {
+      expect(fibLog(6)).toEqual([1, 1, 2, 3, 5, 8])
+      expect(fibLog(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+
+    })
+  })
 
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
@@ -27,7 +57,19 @@
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("oddOnlyLowToHigh", () => {
+    it("returns an array containing the numbers of the Fibonacci sequence up to the number entered as an argument", () => {
+      expect(oddOnlyLowToHigh(fullArr1)).toEqual([-9, 7, 9, 199])
+      expect(oddOnlyLowToHigh(fullArr2)).toEqual([-823, 7, 23])
+
+    })
+  })
+
+
+
 
 const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
@@ -38,10 +80,34 @@ const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 
 // b) Create the function that makes the test pass.
 
+//Pseudo Code
+// filter array by data type to get numbers only 
+// filter numbers to get odd only
+// sort numbers low to high
+
+const oddOnlyLowToHigh = (array) => {
+    const numOnly = (array) => {
+        if (typeof(array) === 'number') {
+            return array
+        }
+    }
+    let numbers = array.filter(numOnly)
+    return numbers.filter(value => value %2 !== 0).sort((a, b)=> a - b)
+}
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("prevNumSum", () => {
+    it("adds the previous value of the array to each new position in the array", () => {
+        expect(prevNumSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(prevNumSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(prevNumSum(numbersToAdd3)).toEqual([])
+
+    })
+  })
 
 const numbersToAdd1 = [2, 4, 45, 9]
 // Excpected output: [2, 6, 51, 60]
@@ -54,3 +120,15 @@ const numbersToAdd3 = []
 
 
 // b) Create the function that makes the test pass.
+
+//Pseudocode 
+//take an array of numbers add then in sequence, n1 + n2 -> n2 + n3 
+//have array = sum of previous array/values
+//.map to run over every value of array 
+// return new array
+
+const prevNumSum = (arr) =>{
+    let array = [],sum
+    array = arr.map(value => sum = (sum || 0 )+ value)
+    return array
+}
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,59 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: `this` refers to the object in which something is being referenced
 
-  Researched answer:
+  Researched answer: `this` is an invoker in JavaScript, when an objects method or value is invoked `this` refers to the object and what it contains. 
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is a library built for JavaScript to simplify the process in building web apps and user interfaces. 
 
-  Researched answer:
+  Researched answer: React.js is an open source JavaScript library used for creating user interfaces, single-page applications - specifically. 
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: this.state refers to the current step a component is at in its lifecycle
 
-  Researched answer:
+  Researched answer: Each component has its own lifecycle, it is the series of methods invoked during different stages of the components entire time invoked. 
 
 
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX is a way to write HTML within JavaScript. A notable difference is that JSX can be HTML written directly into JavaScript opposed to a separate html file. 
 
-  Researched answer:
+  Researched answer: HTML is supported by all browsers but JSX may need to be compiled to 'transpile' JSX into JS the browser can understand. 
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer:Yarn is a package manager, package.json, yarn.lock are the files edited when yarn is ran in the terminal. 
 
-  Researched answer:
+  Researched answer: Yarn is a package manager for Node.JS that focuses on speed, security and consistency. It changes the package.json and yarn.lock files.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: a function without a name or a generic variable name 
 
-  Researched answer:
+  Researched answer: An anonymous function is a type of function with no name and is declared without any identifier -- allows for the immediate execution of a function without needing to be called/invoked
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Using conditional if/else statements to determine if a component is rendered 
 
-2. Object-oriented programming:
+2. Object-oriented programming: Making properties and methods specific to an object, safe from being interfered with. And able to pass down to new objects.
 
-3. Ruby:
+3. Ruby: Ruby is a framework used for web development to make it easier to create more code by writing less 
 
-4. Ruby blocks:
+4. Ruby blocks: Anonymous functions that can be passed into methods 
 
-5. Ruby hashes:
+5. Ruby hashes: Hashes are collections of key-value pairs, similar to an array.
